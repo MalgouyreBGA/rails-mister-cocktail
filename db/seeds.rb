325353d46d70931ecb74d774b5545ad0c6494b79
Bastien Malgouyres <bmalgouyres@free.fr>
2020-08-07 17:05:07 +0200

    css lacking

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+i1 = Ingredient.create(name: "lemon")
+i2 = Ingredient.create(name: "ice")
+i3 = Ingredient.create(name: "mint leaves")
+
+c1 = Cocktail.create(name: "diabolo menthe")
+c2 = Cocktail.create(name: "limonade")
+
+d1 = Dose.create(description: "green", cocktail_id: c1.id, ingredient_id: i3.id)
